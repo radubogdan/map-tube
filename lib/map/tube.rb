e060e55209ce1bb3ef376d3da0226c96da1ea994
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2015-12-28 14:31:09 +0100

    Add dependency and implement Tube

--- lib/map/tube.rb
+++ lib/map/tube.rb
@@ -1,7 +1,14 @@
 require "map/tube/version"
+require "nokogiri"
+require "map/tube/node"
+require "map/tube/graph"
+require "map/tube/parser"
 
 module Map
   module Tube
-    # Your code goes here...
+    def self.new(xml_file_path)
+      doc = File.open(xml_file_path) { |f| Nokogiri::XML(f) }
+      Parser.new(doc).parse!
+    end
   end
 end
