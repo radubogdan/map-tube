ebc72e5efa82c6944d45d4408544436fc09c73b2
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2015-12-28 14:30:22 +0100

    Add first spec and fixture file

--- spec/map/tube_spec.rb
+++ spec/map/tube_spec.rb
@@ -1,11 +1,9 @@
 require 'spec_helper'
 
 describe Map::Tube do
-  it 'has a version number' do
-    expect(Map::Tube::VERSION).not_to be nil
-  end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe "#initialize" do
+    it "takes a xml file and returns a valid Map::Tube::Graph object" do
+      expect(Map::Tube.new("spec/fixtures/bucharest-map.xml").class).to eq Map::Tube::Graph
+    end
   end
 end
