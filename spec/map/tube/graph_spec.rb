1b3491421ea6e6ef47a2490256079045dfba6f2e
Radu-Bogdan Croitoru <croitoruradubogdan@gmail.com>
2015-12-29 13:22:17 +0100

    Add specs for #get_line_by_id and #get_line_by_name

--- spec/map/tube/graph_spec.rb
+++ spec/map/tube/graph_spec.rb
@@ -10,30 +10,58 @@ describe Map::Tube::Graph do
       expect(subject.stations.count).to eq 1
     end
   end
+
   describe "#add_line" do
     it "should add a new line in the graph" do
       subject.add_line(line)
       expect(subject.lines.count).to eq 1
     end
   end
+
   describe "#get_station_by_id" do
     it "should return a station with that id if station exists" do
       subject.add_station(station)
       expect(subject.get_station_by_id("1").id).to eq "1"
     end
-    it "should return an exception if station does not exist" do
+    it "should return an exception if station with that id does not exist" do
       subject.add_station(station)
       expect { subject.get_station_by_id("2") }.to raise_error(Map::Tube::Exceptions::StationError)
     end
   end
+
   describe "#get_station_by_name" do
     it "should return a station with that id if station exists" do
       subject.add_station(station)
       expect(subject.get_station_by_name("Test station").name).to eq "Test station"
     end
-    it "should return an exception if station does not exist" do
+    it "should return an exception if station with that name does not exist" do
       subject.add_station(station)
       expect { subject.get_station_by_name("Another Station") }.to raise_error(Map::Tube::Exceptions::StationError)
     end
   end
+
+  describe "#get_line_by_id" do
+    it "should return a line with that id if line exists" do
+      subject.add_line(line)
+      expect(subject.get_line_by_id("1").id).to eq "1"
+    end
+
+    it "should return an exception if line with that id does not exist" do
+      subject.add_line(line)
+      expect { subject.get_line_by_id("2") }.to raise_error(Map::Tube::Exceptions::LineError)
+    end
+  end
+
+  describe "#get_line_by_name" do
+    it "should return a line with that name if line exists" do
+      subject.add_line(line)
+      expect(subject.get_line_by_name("Line 1").name).to eq "Line 1"
+    end
+
+    it "should return an exception if line with that name does not exist" do
+      subject.add_line(line)
+      expect { subject.get_line_by_name("Line 2") }.to raise_error(Map::Tube::Exceptions::LineError)
+    end
+  end
+
 end
